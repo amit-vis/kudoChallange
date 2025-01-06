53a7fc1166c91c29124955974e7dd833708a2130
amit-vis <amit200698@gmail.com>
2025-01-07 01:57:04 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -5,8 +5,24 @@ require('./config/database')
 const app = express();
 const port = 8000;
 
+const allowedOrigins = [
+    'http://localhost:3000', // Local Development
+    'https://kudo-challange.vercel.app/', // Staging
+    'https://kudochallange.onrender.com' // Production
+];
+
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use('/api', require('./routes/index'))
